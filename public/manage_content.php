ce31a1ce714a3c669551893edafd82e7c377c56f
Musa Sibarani <32220501+musasibarani@users.noreply.github.com>
2017-11-10 02:38:40 +0700

    104 - Moving the navigation to a function
    Tugas week 6

--- public/manage_content.php
+++ public/manage_content.php
@@ -2,63 +2,26 @@
 <?php require_once("../includes/functions.php"); ?>
 <?php include("../includes/layouts/header.php"); ?>
 
-<?
-if (issues ($_GET["subject"])){
-	$selected_subject_id = &_GET["subject"];
+<?php
+if (isset($_GET["subject"])){
+	$selected_subject_id = $_GET ["subject"];
 	$selected_page_id = null;
-}
-else if (issues($_GET["page"])){
+}else if (isset($_GET["page"])){
 	$selected_subject_id = null;
-	$selected_page_id = $_GET ["page"];
-}
-else {
+	$selected_page_id = $_GET["page"];
+}else {
 	$selected_subject_id = null;
 	$selected_page_id = null;
 }
-?>
+
 <div id="main">
 	<div id="navigation">
-		<ul class="subjects">
-		<?php $subject_set = find_all_subjects(); ?>
-		<?php
-		while($subject = mysqli_fetch_assoc($subject_set)) {
-		?>  
-		//	<?php
-		//		echo "<li";
-		//		if($subject["id"] == $selected_subject_id){
-		//			echo " class=\"selected\"";
-		//		}
-		//		echo ">";
-		//	?>
-			<li>
-				<?php echo $subject["menu_name"]; ?>
-				<?php $page_set = find_pages_for_subject($subject["id"]); ?>
-				<ul class="pages">
-				<?php
-					while($page = mysqli_fetch_assoc($page_set)) {
-				?>  
-		//		<?php
-		//			echo "<li";
-		//			if($page["id"] == $selected_page_id){
-		//				echo " class=\"selected\"";
-		//			}
-		//			echo ">";
-		//		?> 
-					<li><?php echo $page["menu_name"]; ?></li>
-				<?php
-					}
-				?>
-				<?php mysqli_free_result($page _set); ?>
-				</ul>
-			</li>
-		<?php
-			}
-		?>
-		<?php mysqli_free_result($subject_set); ?>
-		</ul>
+		<?php echo navigation($selected_subject_id, $selected_page_id); ?>
 	</div>
 	<div id="page">
 		<h2>Manage Content</h2>
+		<? php echo $selected_subject_id; ?><br />
+		<? php echo $selected_page_id; ?>
 	</div>
 </div>
 
