c5b0a30fa5d19dce4c66b63c0966db5aebc4537d
Musa Sibarani <32220501+musasibarani@users.noreply.github.com>
2017-11-10 04:52:41 +0700

    107 - Creating a new subject form
    Tugas week 6

--- public/manage_content.php
+++ public/manage_content.php
@@ -6,17 +6,17 @@
 <div id="main">
 	<div id="navigation">
 		<?php echo navigation($current_subject, $current_page); ?>
+		<br/>
+		<a href="new_subject.php">+ Add a subject</a>
 	</div>
 	<div id="page">
-		<?php if ($current_subject) { ?>
-			<h2>Manage Content</h2>	
-			Menu name: <? php echo $current_subject["menu_name"]; ?><br />
-			
-		<?php } else if ($current_page) { ?>
-			<h2>Manage Page </h2>	
-			Menu name: <? php echo $current_page["menu_name"]; ?><br />
-		
-		<?php } else { ?>
+		<h2>Manage Subject</h2>
+		<?php if ($current_subject){ ?>
+			Menu name: <?php echo $current_subject["menu_name"]; ?><br />
+		<h2>Manage Page</h2>
+		<?php } elseif ($current_page) { ?>
+			Menu name: <?php echo $current_page["menu_name"]; ?><br />
+		<?php } else { ?> 
 			Please select a subject or a page.
 		<?php }?>
 	</div>
