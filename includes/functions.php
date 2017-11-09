42b11a1f915b3bbfd6633a2a0c9143a5ca0b6903
Musa Sibarani <32220501+musasibarani@users.noreply.github.com>
2017-11-10 05:18:10 +0700

    108 - Processing form values and adding subject
    Tugas week 6

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,16 @@
 <?php
-
+	function redirect_to($new_location){
+		header("Location: " . $new_location);
+		exit;
+	}
+	
+	function mysql_prep($string){
+		global $connection;
+		
+		$escaped_string = mysqli_query($connection, $string);
+		return $escaped_string;
+	}
+	
 	function confirm_query($result_set) {
 		if (!$result_set) {
 			die("Database query failed.");
@@ -21,18 +32,16 @@
 	function find_pages_for_subject($subject_id) {
 		global $connection;
 		
-		$safe_subject_id = mysqli_real_escape_string($connection, $subject_id);
-		
 		$query = "SELECT * ";
 		$query .= "FROM pages  ";
-		$query .= "AND subject_id = {$safe_subject_id} ";
+		$query .= "AND subject_id = {$subject_id} ";
 		$query .= "WHERE visible = 1 ";
 		$query .= "ORDER BY position ASC";
 		$page_set = mysqli_query($connection, $query);
 		confirm_query($page_set);
 		return $page_set; 
 	}
-	
+
 	function find_subject_by_id($subject_id){
 		global $connection;
 		
@@ -40,15 +49,15 @@
 		
 		$query = "SELECT * ";
 		$query .= "FROM subjects ";
-		$query .= "WHERE id = {$subject_id} ";
+		$query .= "WHERE id = {$safe_subject_id} ";
 		$query .= "LIMIT 1";
 		$subject_set = mysqli_query($connection, $query);
 		confirm_query($subject_set);
-		if ($subject = mysql_fetch_assoc ($subject_set)) {
-			return $subject;		
+		if($subject = mysqli_fetch_assoc($subject_set)){
+			return $subject;
 		} else {
 			return null;
-		}
+		}		
 	}
 	
 	function find_page_by_id($page_id){
@@ -56,43 +65,60 @@
 		
 		$safe_page_id = mysqli_real_escape_string($connection, $page_id);
 		
-		$query = "SELECT *";
-		$query .= "FROM subjects ";
+		$query = "SELECT * ";
+		$query .= "FROM page ";
 		$query .= "WHERE id = {$safe_page_id} ";
 		$query .= "LIMIT 1";
-		$subject_set = mysqli_query($connection, $query);
+		$page_set = mysqli_query($connection, $query);
 		confirm_query($page_set);
-		if ($subject = mysql_fetch_assoc ($page_set)) {
-			return $page;		
+		if($page = mysqli_fetch_assoc($page_set)){
+			return $page;
 		} else {
 			return null;
-		}
+		}		
 	}
 	
-	function find_selected_page() {
+	function find_selected_page(){
 		global $current_subject;
 		global $current_page;
 		
-	if (isset($_GET["subject"])){
-		$current_subject = find_subject_by_id($_GET ["subject"]);
-		$current_page = null;
-	}	else if (isset($_GET["page"])){
-		$current_subject = null;
-		$current_subject = find_subject_by_id($_GET["page"]);
-	}	else {
-		$current_subject = null;
-		$current_page = null;
+		if (isset($_GET["subject"])){
+			$current_subject = find_subject_by_id($_GET ["subject"]);
+			$current_page = null;
+		}else if (isset($_GET["page"])){
+			$current_subject = null;
+			$current_page = find_subject_by_id($_GET["page"];
+		}else {
+			$current_subject = null;
+			$current_page = null;
+		}
 	}
-}
+
+		global $connection;
+		
+		$safe_subject_id = mysqli_real_escape_string($connection, $subject_id);
+		
+		$query = "SELECT * ";
+		$query .= "FROM subjects ";
+		$query .= "WHERE id = {$safe_subject_id} ";
+		$query .= "LIMIT 1";
+		$subject_set = mysqli_query($connection, $query);
+		confirm_query($subject_set);
+		if($subject = mysqli_fetch_assoc($subject_set)){
+			return $subject;
+		} else {
+			return null;
+		}		
+	}
 	// navigation takes 2 arguments
 	// - the current subject array or null
 	// - the current page array or null
-	function navigation($subject_array, $page_array){		
+	function navigation($subject_array, $page_array){
 		$output = "<ul class=\"subjects\">";
 		$subject_set = find_all_subjects();
 		while($subject = mysqli_fetch_assoc($subject_set)) {
 		$output .= "<li";
-		if ($subject_array && $subject["id"] == $subject_array["id"]){
+		if($subject_array && $subject["id"] == $subject_array["id"]){
 					$output .= " class=\"selected\"";
 				}
 				$output .= ">";
@@ -101,7 +127,6 @@
 				$output .= "\">";
 				$output .= $subject["menu_name"];
 				$output .= "</a>";
-				
 				$page_set = find_pages_for_subject($subject["id"]);
 				$output .= "<ul class=\"pages\">";
 				while($page = mysqli_fetch_assoc($page_set)) {
